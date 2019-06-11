c3a0bcc657f8f39cd31fa7ad166afceccec29cad
Bastian Köcher <git@kchr.de>
2019-06-11 15:43:06 +0200

    POC2

--- src/bin/parse.rs
+++ src/bin/parse.rs
@@ -1,6 +1,8 @@
 use parity_codec::{Decode, Encode};
 use reflection::{Reflection, TypeInfo};
 
+use std::any::TypeId;
+
 #[derive(Debug, Encode)]
 struct Node<T> {
     data: T,
@@ -8,19 +10,40 @@ struct Node<T> {
 }
 
 impl<T: Reflection> Reflection for Node<T> {
-    fn get_type_info() -> Vec<TypeInfo> {
-        let mut data = T::get_type_info();
-        data.insert(0, TypeInfo::SelfRefStructStart);
-        data.push(TypeInfo::Optional);
-        data.push(TypeInfo::SelfRef);
-        data.push(TypeInfo::StructEnd);
-        data
+    fn get_type_info_into_impl(infos: &mut Vec<TypeInfo>, parents: &mut Vec<(TypeId, u8)>) {
+        infos.push(TypeInfo::StructStart);
+        T::get_type_info_into(infos, parents);
+        Option::<Box<Self>>::get_type_info_into(infos, parents);
+        infos.push(TypeInfo::StructEnd);
+    }
+}
+
+#[derive(Debug, Encode)]
+struct A(Option<Box<B>>);
+
+impl Reflection for A {
+    fn get_type_info_into_impl(infos: &mut Vec<TypeInfo>, parents: &mut Vec<(TypeId, u8)>) {
+        infos.push(TypeInfo::StructStart);
+        Option::<Box<B>>::get_type_info_into(infos, parents);
+        infos.push(TypeInfo::StructEnd);
+    }
+}
+
+#[derive(Debug, Encode)]
+struct B(A, Option<Box<B>>);
+
+impl Reflection for B {
+    fn get_type_info_into_impl(infos: &mut Vec<TypeInfo>, parents: &mut Vec<(TypeId, u8)>) {
+        infos.push(TypeInfo::StructStart);
+        A::get_type_info_into(infos, parents);
+        Option::<Box<B>>::get_type_info_into(infos, parents);
+        infos.push(TypeInfo::StructEnd);
     }
 }
 
 #[derive(Debug)]
 struct Context<'a> {
-    self_ref: Option<&'a [TypeInfo]>,
+    orig: &'a [TypeInfo],
 }
 
 fn main() {
@@ -30,7 +53,10 @@ fn main() {
             data: 2u32,
             next: Some(Box::new(Node {
                 data: 3u32,
-                next: None,
+                next: Some(Box::new(Node {
+                    data: 4u32,
+                    next: None,
+                })),
             })),
         })),
     };
@@ -43,10 +69,25 @@ fn main() {
 
     println!("Orig: {:?}", data);
     println!("Decoded: {}", decoded_type_info);
+
+    let data2 = B(
+        A(Some(Box::new(B(A(None), None)))),
+        Some(Box::new(B(A(None), None))),
+    );
+
+    let encoded2 = data2.encode();
+
+    println!("Typeinfo: {:?}", B::get_type_info());
+    println!("encoded: {:?}", encoded2);
+
+    let decoded_type_info = decode(&B::get_type_info(), &mut &encoded2[..]);
+
+    println!("Orig: {:?}", data2);
+    println!("Decoded: {}", decoded_type_info);
 }
 
 fn decode(type_info: &[TypeInfo], encoded: &mut &[u8]) -> String {
-    let mut context = Context { self_ref: None };
+    let mut context = Context { orig: type_info };
 
     let mut res = String::new();
     let mut i = 0;
@@ -66,21 +107,21 @@ fn decode(type_info: &[TypeInfo], encoded: &mut &[u8]) -> String {
     res
 }
 
-fn decode_self_ref_struct<'a>(
+fn decode_struct<'a>(
     type_info: &'a [TypeInfo],
     encoded: &mut &[u8],
     context: &mut Context<'a>,
     consumed: &mut usize,
 ) -> String {
-    let old = context.self_ref;
-    context.self_ref = Some(type_info);
-
     let mut i = 0;
     let mut res = String::new();
     while i < type_info.len() {
         let mut inner_consumed = 0;
         match type_info[i] {
-            TypeInfo::StructEnd => break,
+            TypeInfo::StructEnd => {
+                *consumed += 1;
+                break;
+            }
             o => {
                 res.push_str(&decode_type_info(
                     o,
@@ -96,7 +137,6 @@ fn decode_self_ref_struct<'a>(
         i += inner_consumed;
         *consumed += inner_consumed;
     }
-    context.self_ref = old;
     res
 }
 
@@ -130,25 +170,53 @@ fn decode_type_info<'a>(
                 "None".to_string()
             }
         }
-        TypeInfo::SelfRefStructStart => decode_self_ref_struct(rest, encoded, context, consumed),
-        TypeInfo::SelfRef => {
-            let inner = context
-                .self_ref
-                .expect("Needs to be set when we encounter a `SelfRef`.");
-            decode_self_ref_struct(inner, encoded, context, consumed)
+        TypeInfo::StructStart => decode_struct(rest, encoded, context, consumed),
+        TypeInfo::RefType(index) => {
+            let index = index as usize;
+            let mut consumed = 0;
+            decode_type_info(
+                context.orig[index],
+                &context.orig[index + 1..],
+                encoded,
+                context,
+                &mut consumed,
+            )
         }
-        _ => unimplemented!(),
+        t => unimplemented!("{:?}", t),
     }
 }
 
 fn skip_type(type_info: &[TypeInfo], consumed: &mut usize) {
     for i in 0..type_info.len() {
         match type_info[i] {
-            TypeInfo::SelfRef => {
+            TypeInfo::RefType(_) => {
                 *consumed += 1;
                 break;
             }
-            _ => unimplemented!(),
+            TypeInfo::StructStart => {
+                let mut count = 0;
+                let count = type_info[i + 1..]
+                    .iter()
+                    .position(|t| match t {
+                        TypeInfo::StructStart => {
+                            count += 1;
+                            false
+                        }
+                        TypeInfo::StructEnd => {
+                            if count > 0 {
+                                count -= 1;
+                                false
+                            } else {
+                                true
+                            }
+                        }
+                        _ => false,
+                    })
+                    .expect("Struct end always exists!");
+                *consumed += count + 2;
+                break;
+            }
+            i => unimplemented!("{:?}", i),
         }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
+use std::any::TypeId;
+
 #[derive(Debug, PartialEq, Copy, Clone)]
 pub enum TypeInfo {
     U8,
     I8,
     U32,
     StructStart,
-    SelfRefStructStart,
-    SelfRef,
     StructEnd,
     Optional,
     Enum,
@@ -15,56 +15,70 @@ pub enum TypeInfo {
     EnumVariantUnit,
     List,
     Void,
+    RefType(u8),
 }
 
-pub trait Reflection {
+pub trait Reflection: 'static {
     /// TODO: I don't want a `Vec` as return value.
-    fn get_type_info() -> Vec<TypeInfo>;
+    fn get_type_info() -> Vec<TypeInfo> {
+        let mut infos = Vec::new();
+        let mut parents = Vec::new();
+        Self::get_type_info_into(&mut infos, &mut parents);
+        infos
+    }
+    fn get_type_info_into(infos: &mut Vec<TypeInfo>, parents: &mut Vec<(TypeId, u8)>) {
+        if let Some(parent) = parents.iter().find(|i| i.0 == TypeId::of::<Self>()) {
+            infos.push(TypeInfo::RefType(parent.1));
+        } else {
+            parents.push((TypeId::of::<Self>(), infos.len() as u8));
+            Self::get_type_info_into_impl(infos, parents);
+            parents.pop();
+        }
+    }
+    fn get_type_info_into_impl(infos: &mut Vec<TypeInfo>, parents: &mut Vec<(TypeId, u8)>);
 }
 
 impl Reflection for u8 {
-    fn get_type_info() -> Vec<TypeInfo> {
-        vec![TypeInfo::U8]
+    fn get_type_info_into_impl(infos: &mut Vec<TypeInfo>, _: &mut Vec<(TypeId, u8)>) {
+        infos.push(TypeInfo::U8);
     }
 }
 
 impl Reflection for i8 {
-    fn get_type_info() -> Vec<TypeInfo> {
-        vec![TypeInfo::I8]
+    fn get_type_info_into_impl(infos: &mut Vec<TypeInfo>, _: &mut Vec<(TypeId, u8)>) {
+        infos.push(TypeInfo::I8);
     }
 }
 
 impl Reflection for u32 {
-    fn get_type_info() -> Vec<TypeInfo> {
-        vec![TypeInfo::U32]
+    fn get_type_info_into_impl(infos: &mut Vec<TypeInfo>, _: &mut Vec<(TypeId, u8)>) {
+        infos.push(TypeInfo::U32);
     }
 }
 
 impl<T: Reflection> Reflection for Box<T> {
-    fn get_type_info() -> Vec<TypeInfo> {
-        T::get_type_info()
+    fn get_type_info_into_impl(infos: &mut Vec<TypeInfo>, parents: &mut Vec<(TypeId, u8)>) {
+        T::get_type_info_into_impl(infos, parents)
     }
 }
 
 impl<T: Reflection> Reflection for Vec<T> {
-    fn get_type_info() -> Vec<TypeInfo> {
-        let mut data = vec![TypeInfo::List];
-        data.extend(T::get_type_info());
-        data
+    fn get_type_info_into_impl(infos: &mut Vec<TypeInfo>, parents: &mut Vec<(TypeId, u8)>) {
+        infos.push(TypeInfo::List);
+        T::get_type_info_into_impl(infos, parents);
     }
 }
 
 impl Reflection for () {
-    fn get_type_info() -> Vec<TypeInfo> {
-        vec![TypeInfo::Void]
+    fn get_type_info_into_impl(infos: &mut Vec<TypeInfo>, _: &mut Vec<(TypeId, u8)>) {
+        infos.push(TypeInfo::Void);
     }
 }
 
 impl<T: Reflection> Reflection for Option<T> {
-    fn get_type_info() -> Vec<TypeInfo> {
-        let mut data = T::get_type_info();
-        data.insert(0, TypeInfo::Optional);
-        data
+    fn get_type_info_into_impl(infos: &mut Vec<TypeInfo>, parents: &mut Vec<(TypeId, u8)>) {
+        infos.push(TypeInfo::Optional);
+        T::get_type_info_into_impl(infos, parents);
     }
 }
 
@@ -77,14 +91,12 @@ mod tests {
         next: Option<Box<Node<T>>>,
     }
 
-    impl<T: Reflection> Reflection for Node<T> {
-        fn get_type_info() -> Vec<TypeInfo> {
-            let mut data = T::get_type_info();
-            data.insert(0, TypeInfo::SelfRefStructStart);
-            data.push(TypeInfo::Optional);
-            data.push(TypeInfo::SelfRef);
-            data.push(TypeInfo::StructEnd);
-            data
+    impl<T: Reflection + 'static> Reflection for Node<T> {
+        fn get_type_info_into_impl(infos: &mut Vec<TypeInfo>, parents: &mut Vec<(TypeId, u8)>) {
+            infos.insert(0, TypeInfo::StructStart);
+            T::get_type_info_into_impl(infos, parents);
+            Option::<Box<Node<T>>>::get_type_info_into_impl(infos, parents);
+            parents.pop();
         }
     }
 
@@ -95,17 +107,10 @@ mod tests {
     }
 
     impl<T: Reflection> Reflection for Node2<T> {
-        fn get_type_info() -> Vec<TypeInfo> {
-            let mut data = T::get_type_info();
-            data.insert(0, TypeInfo::StructStart);
-            data.push(TypeInfo::Optional);
-            data.push(TypeInfo::SelfRefStructStart);
-            data.extend(u32::get_type_info());
-            data.push(TypeInfo::Optional);
-            data.push(TypeInfo::SelfRef);
-            data.push(TypeInfo::StructEnd);
-            data.push(TypeInfo::StructEnd);
-            data
+        fn get_type_info_into_impl(infos: &mut Vec<TypeInfo>, parents: &mut Vec<(TypeId, u8)>) {
+            infos.push(TypeInfo::StructStart);
+            Option::<Box<Node<u32>>>::get_type_info_into(infos, parents);
+            infos.push(TypeInfo::StructEnd);
         }
     }
 
@@ -115,12 +120,11 @@ mod tests {
     }
 
     impl<T: Reflection> Reflection for SomeStruct<T> {
-        fn get_type_info() -> Vec<TypeInfo> {
-            let mut data = vec![TypeInfo::StructStart];
-            data.extend(u8::get_type_info());
-            data.extend(T::get_type_info());
-            data.push(TypeInfo::StructEnd);
-            data
+        fn get_type_info_into_impl(infos: &mut Vec<TypeInfo>, parents: &mut Vec<(TypeId, u8)>) {
+            infos.push(TypeInfo::StructStart);
+            u8::get_type_info_into(infos, parents);
+            T::get_type_info_into(infos, parents);
+            infos.push(TypeInfo::StructEnd);
         }
     }
 
@@ -131,20 +135,16 @@ mod tests {
     }
 
     impl Reflection for SomeEnum {
-        fn get_type_info() -> Vec<TypeInfo> {
-            let mut data = vec![
-                TypeInfo::Enum,
-                TypeInfo::EnumVariantUnit,
-                TypeInfo::EnumVariantStart,
-                TypeInfo::U32,
-                TypeInfo::I8,
-                TypeInfo::EnumVariantEnd,
-                TypeInfo::EnumVariantStart,
-            ];
-
-            data.extend(SomeStruct::<u32>::get_type_info());
-            data.push(TypeInfo::EnumVariantEnd);
-            data
+        fn get_type_info_into_impl(infos: &mut Vec<TypeInfo>, parents: &mut Vec<(TypeId, u8)>) {
+            infos.push(TypeInfo::Enum);
+            infos.push(TypeInfo::EnumVariantUnit);
+            infos.push(TypeInfo::EnumVariantStart);
+            u32::get_type_info_into(infos, parents);
+            i8::get_type_info_into(infos, parents);
+            infos.push(TypeInfo::EnumVariantEnd);
+            infos.push(TypeInfo::EnumVariantStart);
+            SomeStruct::<u32>::get_type_info_into(infos, parents);
+            infos.push(TypeInfo::EnumVariantEnd);
         }
     }
 }
